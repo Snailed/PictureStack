ff8cd47f934fdee8ba785e8779f9406f4a5744c9
RasmusLøvstad <rasmus.hag.lovstad@gmail.com>
2016-07-27 19:56:30 +0200

    Tilføjet et par klasser, gør klar til at bygge upload-featuren

--- app/src/main/java/lovstad/dk/picturestack/Intro_akt.java
+++ app/src/main/java/lovstad/dk/picturestack/Intro_akt.java
@@ -2,34 +2,21 @@ package lovstad.dk.picturestack;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class Intro_akt extends AppCompatActivity implements View.OnClickListener {
     public static ImageView testbillede;
     public static TextView testtext;
     public static ArrayList<String> billeder;
-    public int billedcount = 0;
-    public Bitmap placeholder;
+    public int billedcount = 1;
+    public Bitmap placeholder = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +25,14 @@ public class Intro_akt extends AppCompatActivity implements View.OnClickListener
         testtext = (TextView) findViewById(R.id.testtext);
 
         if (placeholder == null) {
-            placeholder = BitmapFactory.decodeFile("loadingplaceholder.png");
 
+            testbillede.setImageResource(R.drawable.loadingplaceholder);
+
+
+        } else {
+            testbillede.setImageBitmap(placeholder);
         }
-        testbillede.setImageBitmap(placeholder);
+        //testbillede.setImageBitmap(placeholder);
 
 
         Log.d("AsyncTask", "Asynctask starter om lidt...");
@@ -55,6 +46,7 @@ public class Intro_akt extends AppCompatActivity implements View.OnClickListener
     protected static void setData(ArrayList<String> data) {
         billeder = data;
         testtext.setText("Billeder:");
+        testbillede.setImageBitmap(Base64crypter.decode(billeder.get(0)));
 
     }
 
---
+++ app/src/main/java/lovstad/dk/picturestack/Kamera.java
@@ -0,0 +1,7 @@
+package lovstad.dk.picturestack;
+
+/**
+ * Created by bruger on 27-07-2016.
+ */
+public class Kamera {
+}
